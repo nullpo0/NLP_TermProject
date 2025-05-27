7fb61cdf091a8f73544c24b3c298c9c007b6afb2
CHJPC\liker <likercho63@gmail.com>
2025-05-27 19:21:18 +0900

    getting started

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "hello"}
+    
+@app.get("/response")
+async def response():
+    return {"response": "Done!"}
